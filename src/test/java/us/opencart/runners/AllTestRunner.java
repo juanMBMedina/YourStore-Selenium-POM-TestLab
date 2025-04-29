05450912ca36fcf8911a36d3b7e444de072dabc6
Juan Miguel <juanmblancom@gmail.com>
2025-04-29 05:55:40 -0500

    Testing Pipelines

---
+++ src/test/java/us/opencart/runners/AllTestRunner.java
@@ -0,0 +1,21 @@
+package us.opencart.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/opencart",
+        glue = {"us/opencart/stepDefinitions", "us/opencart/hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports/cucumber.json"
+        },
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        tags =  "@your_store_add_to_cart_feature @your_store_login_feature @your_store_register_feature"
+)
+public class AllTestRunner {
+}
